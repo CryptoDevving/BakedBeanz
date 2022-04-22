b7dac33b56a60b5fedc00174ee075a48806598a3
CryptoDevving <36123966+CryptoDevving@users.noreply.github.com>
2022-04-22 18:11:57 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { WalletConnectConnector, URI_AVAILABLE  } from '@web3-react/walletconnect-connector'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
